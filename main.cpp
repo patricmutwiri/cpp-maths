95233e26a2900e0e2dc35c59170971f2b6df12fb
Patrick Mutwiri <patwiri@gmail.com>
2020-02-07 10:18:22 +0300

    all maths test :)

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    float x,y, product,sum,divs,subs;
+    cout << "Enter x ";
+    cin>>x;
+    cout << "Enter y ";
+    cin>>y;
+
+        product = x*y;
+
+
+        divs = x/y;
+
+
+        subs = x-y;
+
+
+        sum = x+y;
+
+
+
+    cout<<"The output of "<<x<<" and "<<y<<" is ";
+    cout<<"Sum "<<sum<<"\nProduct "<<product<<"\nDivision"<<divs<<"\nSubs"<<subs;
+
+    return 0;
+}
